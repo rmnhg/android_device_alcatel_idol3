246db5e3a5fe7e221cbec2a5ad8f41bfea165d1a
Ramón Hernández García <rmnhg16@gmail.com>
2017-08-10 00:55:46 +0200

    Vendor

--- aosp_idol3.mk
+++ aosp_idol3.mk
@@ -17,17 +17,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# Inherit from idol3 device
-$(call inherit-product, device/alcatel/idol3/device.mk)
-
-
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Include package config fragments
 include $(LOCAL_PATH)/product/*.mk
 
-$(call inherit-product-if-exists, vendor/alcatel/idol3/idol3-vendor.mk)
+$(call inherit-product, vendor/alcatel/idol3/idol3-vendor.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := idol3
